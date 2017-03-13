90751b7d89a333025136098a3422bdeb962acf21
Jean-Claude Charrier <charrierjeanclaude@yahoo.com>
2017-03-12 21:49:30 -0700

    baseline in myteam

--- Tournament/myTeam.py
+++ Tournament/myTeam.py
@@ -7,16 +7,18 @@
 # For more info, see http://inst.eecs.berkeley.edu/~cs188/sp09/pacman.html
 
 from captureAgents import CaptureAgent
+import distanceCalculator
 import random, time, util
 from game import Directions
 import game
+from util import nearestPoint
 
 #################
 # Team creation #
 #################
 
 def createTeam(firstIndex, secondIndex, isRed,
-               first = 'DummyAgent', second = 'DummyAgent'):
+               first = 'OffensiveReflexAgent', second = 'DefensiveReflexAgent'):
   """
   This function should return a list of two agents that will form the
   team, initialized using firstIndex and secondIndex as their agent
@@ -39,48 +41,127 @@ def createTeam(firstIndex, secondIndex, isRed,
 # Agents #
 ##########
 
-class DummyAgent(CaptureAgent):
-  """
-  A Dummy agent to serve as an example of the necessary agent structure.
-  You should look at baselineTeam.py for more details about how to
-  create an agent as this is the bare minimum.
-  """
+# class DummyAgent(CaptureAgent):
+#   """
+#   A Dummy agent to serve as an example of the necessary agent structure.
+#   You should look at baselineTeam.py for more details about how to
+#   create an agent as this is the bare minimum.
+#   """
 
-  def registerInitialState(self, gameState):
-    """
-    This method handles the initial setup of the
-    agent to populate useful fields (such as what team
-    we're on). 
+#   def registerInitialState(self, gameState):
+#     """
+#     This method handles the initial setup of the
+#     agent to populate useful fields (such as what team
+#     we're on). 
     
-    A distanceCalculator instance caches the maze distances
-    between each pair of positions, so your agents can use:
-    self.distancer.getDistance(p1, p2)
+#     A distanceCalculator instance caches the maze distances
+#     between each pair of positions, so your agents can use:
+#     self.distancer.getDistance(p1, p2)
 
-    IMPORTANT: This method may run for at most 15 seconds.
-    """
+#     IMPORTANT: This method may run for at most 15 seconds.
+#     """
 
-    ''' 
-    Make sure you do not delete the following line. If you would like to
-    use Manhattan distances instead of maze distances in order to save
-    on initialization time, please take a look at
-    CaptureAgent.registerInitialState in captureAgents.py. 
-    '''
-    CaptureAgent.registerInitialState(self, gameState)
+#     ''' 
+#     Make sure you do not delete the following line. If you would like to
+#     use Manhattan distances instead of maze distances in order to save
+#     on initialization time, please take a look at
+#     CaptureAgent.registerInitialState in captureAgents.py. 
+#     '''
+#     CaptureAgent.registerInitialState(self, gameState)
 
-    ''' 
-    Your initialization code goes here, if you need any.
-    '''
+#     ''' 
+#     Your initialization code goes here, if you need any.
+#     '''
 
+
+#   def chooseAction(self, gameState):
+#     """
+#     Picks among actions randomly.
+#     """
+#     actions = gameState.getLegalActions(self.index)
+
+#     ''' 
+#     You should change this in your own agent.
+#     '''
+
+#     return random.choice(actions)
+
+class ReflexCaptureAgent(CaptureAgent):
 
   def chooseAction(self, gameState):
-    """
-    Picks among actions randomly.
-    """
     actions = gameState.getLegalActions(self.index)
+    values = [self.evaluate(gameState, a) for a in actions]
 
-    ''' 
-    You should change this in your own agent.
-    '''
+    maxValue = max(values)
+    bestActions = [a for a, v in zip(actions, values)if v == maxValue]
 
-    return random.choice(actions)
+    return random.choice(bestActions)
 
+  def getSuccessor(self, gameState, action):
+
+    successor = gameState.generateSuccessor(self.index, action)
+    pos = successor.getAgentState(self.index).getPosition()
+    if pos != nearestPoint(pos):
+      return successor.generateSuccessor(self.index, action)
+    else:
+      return successor
+  
+  def evaluate(self, gameState, action):
+    features = self.getFeatures(gameState, action)
+    weights = self.getWeights(gameState, action)
+    return features * weights
+
+  def getFeatures(self, gameState, action):
+
+    features = util.Counter()
+    successor = self.getSuccessor(gameState, action)
+    features['successorScore'] = self.getScore(successor)
+    return features
+
+  def getWeights(self, gameState, action):
+    return {'successorScore': 1.0}
+
+class OffensiveReflexAgent(ReflexCaptureAgent):
+
+  def getFeatures(self, gameState, action):
+    features = util.Counter()
+    successor = self. getSuccessor(gameState, action)
+    features['successorScore'] = self.getScore(successor)
+
+    foodList = self.getFood(successor).asList()
+    if len(foodList) > 0:
+      myPos = successor.getAgentState(self.index).getPosition()
+      minDistance = min([self.getMazeDistance(myPos, food) for food in foodList])
+      features['distanceToFood'] = minDistance
+    return features
+
+  def getWeights(self, gameState, action):
+    return {'successorScore':100, 'distanceToFood': -1}
+
+class DefensiveReflexAgent(ReflexCaptureAgent):
+
+  def getFeatures(self, gameState, action):
+    features = util.Counter()
+    successor = self.getSuccessor(gameState, action)
+
+    myState = successor.getAgentState(self.index)
+    myPos = myState.getPosition()
+
+    features['onDefense'] = 1
+    if myState.isPacman: features['onDefense'] = 0
+
+    enemies = [successor.getAgentState(i) for i in self.getOpponents(successor)]
+    invaders = [a for a in enemies if a.isPacman and a.getPosition() != None]
+    features['numInvaders'] = len(invaders)
+    if len(invaders) > 0:
+      dists = [self.getMazeDistance(myPos, a.getPosition()) for a in invaders]
+      features['invaderDistance'] = min(dists)
+
+    if action == Directions.STOP: features['stop'] = 1
+    rev = Directions.REVERSE[gameState.getAgentState(self.index).configuration.direction]
+    if action == rev: features['reverse'] = 1
+
+    return features
+
+  def getWeights(self, gameState, action):
+    return {'numInvaders': -1000, 'onDefense':100, 'invaderDistance': -10, 'stop': -100, 'reverse': -2}
